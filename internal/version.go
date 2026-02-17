e62ebf8ee28cd05a32a9705c52c3f1a808125b96
Torbjörn Einarsson <torbjorn@einarssons.se>
2026-02-17 20:35:37 +0100

    chore: bump version to v0.9.0

--- internal/version.go
+++ internal/version.go
@@ -7,8 +7,8 @@ import (
 )
 
 var (
-	commitVersion string = "v0.8.0"     // May be updated using build flags
-	commitDate    string = "1739577600" // commitDate in Epoch seconds (may be overridden using build flags)
+	commitVersion string = "v0.9.0"     // May be updated using build flags
+	commitDate    string = "1771282800" // commitDate in Epoch seconds (may be overridden using build flags)
 )
 
 // GetVersion returns version and commitDate if available.
